bbe2b26277b9dca335b25d1eb75770577c7d44b2
ZAHawout <zachary.a.hawout@gmail.com>
2024-06-24 10:52:22 -0400

    Additional cleanup of source files.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
 
 function App() {
-
-return (
-
-<div className="App">
-
-<header>
-
-<h1>Browser Parking Lot</h1>
-
-</header>
-
-</div>
-
+  return (
+    <div className="App">
+      <header>
+        <h1>Browser Parking Lot</h1>
+        <p>Send most of your browser tabs into retirement.</p>
+      </header>
+    </div>
 );
-
 }
 
 export default App;
